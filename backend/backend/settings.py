5a1c1392073eeb5008183a1f660e2f163721c6e3
Arkadiusz Kiszka <00oookiocz@gmail.com>
2024-10-14 23:24:31 +0200

    DOdanie plików docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-we2mbs@atn3srakd0ke!#(jvu(w-y*mg*qck7e0kgr+#qont$3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
